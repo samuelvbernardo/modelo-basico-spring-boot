0c2d2d2aa3273ae9009c124aa314b880cda02749
Samuelvbernardo <samuelbernardo@ads.fiponline.edu.br>
2025-01-05 23:28:28 -0300

    first commit

---
+++ src/main/java/com/kipper/first_spring_app/configuration/HelloConfiguration.java
@@ -0,0 +1,16 @@
+package com.kipper.first_spring_app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloConfiguration {
+
+    //@Bean
+    //public Transport myService(){ -> Exemplo de uso
+    //  return new Car();
+    // }
+
+//Tranport interface
+//Car -> implementação
+}
---
+++ src/main/java/com/kipper/first_spring_app/controller/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.kipper.first_spring_app.controller;
+
+import com.kipper.first_spring_app.domain.User;
+import com.kipper.first_spring_app.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello-world")
+public class HelloWorldController {
+
+    @Autowired
+    private HelloWorldService helloWorldService;
+
+    public HelloWorldController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
+    @GetMapping
+    public String helloWorld(){
+        return helloWorldService.helloWorld("Samuel");
+    }
+
+    @PostMapping("/{id}")
+    public String helloWorldPost(@PathVariable("id") String id, @RequestParam(value = "filter", defaultValue = "nenhum") String filter,@RequestBody User body){
+        return "Hello World" + filter;
+    }
+}
